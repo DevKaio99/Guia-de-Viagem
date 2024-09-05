222944c94ca3f8ef043656ab8e57a0854a1288f0
Prado <kaio.prado@zeiss.com>
2024-09-04 22:24:17 -0300

    Primeiro commit

---
+++ dados.js
@@ -0,0 +1,5 @@
+let dados = {
+    titulo : 'Maresias',
+    descricao : 'Localizada no litoral norte de São Paulo, é um destino que encanta por suas belezas naturais e infraestrutura completa. Além da famosa praia, com suas ondas perfeitas para o surf, a região oferece diversas opções de lazer e aventura. Trilhas exuberantes, como a que liga Maresias a Paúba, revelam paisagens deslumbrantes da Mata ',
+    link : 'Em construção',
+};
