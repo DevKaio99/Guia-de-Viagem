fa348864966b384c8ecafb2840a0ec397f105fb3
Kaio Andrade <kaiokakarma@hotmail.com>
2024-09-05 23:58:16 -0300

    Incluso o código JavaScript para mostrar os resultados ao clicar no botão Pesquisar

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-console.log(dados.titulo);
+
+function pesquisar() {
+    let pesquisa = document.getElementById('resultados-pesquisa');
+    let resultados = '';
+
+for (let dado of dados){
+    resultados +=`
+    <div class="item-resultado">
+    <h2>
+            <a href="#">${dado.titulo}</a></h2>
+    <p class="descricao-meta">${dado.descricao}</p>
+    <a href="#">${dado.link}</a>
+</div>`
+}
+
+pesquisa.innerHTML = resultados;
+}
+
+
+
--- dados.js
+++ dados.js
@@ -1,5 +1,17 @@
-let dados = {
-    titulo : 'Maresias',
-    descricao : 'Localizada no litoral norte de São Paulo, é um destino que encanta por suas belezas naturais e infraestrutura completa. Além da famosa praia, com suas ondas perfeitas para o surf, a região oferece diversas opções de lazer e aventura. Trilhas exuberantes, como a que liga Maresias a Paúba, revelam paisagens deslumbrantes da Mata ',
-    link : 'Em construção',
-};
+let dados = [
+    {
+        titulo: 'Maresias',
+        descricao: 'Localizada no litoral norte de São Paulo, é um destino que encanta por suas belezas naturais e infraestrutura completa. Além da famosa praia, com suas ondas perfeitas para o surf, a região oferece diversas opções de lazer e aventura. Trilhas exuberantes, como a que liga Maresias a Paúba, revelam paisagens deslumbrantes da Mata ',
+        link: 'Mais informações',
+    },
+    {
+        titulo: 'Guarujá',
+        descricao: 'Conhecido por suas diversas praias, Guarujá oferece opções para todos os gostos: desde as águas calmas da Enseada até as ondas fortes de Pitangueiras. Além das praias, a cidade conta com uma rica vida noturna e diversas opções de restaurantes e bares.',
+        link: 'Mais informações',
+    },
+    {
+        titulo: 'Bertioga',
+        descricao: 'Bertioga é um paraíso ecológico, com praias preservadas e uma natureza exuberante. A praia da Riviera de São Lourenço é um dos destaques, com suas águas cristalinas e areia branca. A cidade também oferece diversas opções de trilhas e ecoturismo.',
+        link: 'Mais informações',
+    }
+];
